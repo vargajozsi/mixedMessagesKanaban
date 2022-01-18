9fce460fec978ff9eaf1ebaabe0cd94a0e446aea
vargajozsi <sikloernyos@hotmail.com>
2022-01-18 23:03:06 +0100

    words database for random selection fulfilled

--- gogo.js
+++ gogo.js
@@ -1,1 +1,7 @@
-console.log("wefewfcc")
+const words = {
+    first: ["Jingles", "Holly", "Kringle", "Buddy", "Snowflake", "Twinkle", "Ginger", "Trixie", "Noel", "Tinsel", "Winter", "Rudy"],
+    middle: ["the merry", "the playful", "the frosty", "the festive", "the jolly", "the little", "the snowy", "the nice", "the glittery", "the sparkly", "the joyvial", "the lively"],
+    end: ["Angel", "Evergreen", "Ornament", "Tinsel", "Cookie", "Snowman", "Gumdrop", "Sleigh bells", "Cupcake", "Reindeer", "Candy cane", "Garland"]
+}
+
+console.log(...words.first);
